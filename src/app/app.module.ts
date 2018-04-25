a6049346c4774ceb4b7248eda5f7806c972057d2
Sutthinart Khunvadhana <iakgoog@gmail.com>
2018-04-25 15:37:18 +0700

    Done HTTP SEARCH by name

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,9 +14,10 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService } from './in-memory-data.service';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
 
 @NgModule({
-  declarations: [AppComponent, HeroesComponent, HeroDetailComponent, MessagesComponent, DashboardComponent],
+  declarations: [AppComponent, HeroesComponent, HeroDetailComponent, MessagesComponent, DashboardComponent, HeroSearchComponent],
   imports: [
     BrowserModule,
     FormsModule,
